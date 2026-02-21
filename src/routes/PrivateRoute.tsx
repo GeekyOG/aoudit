67a7f1b7a378f5b7a28ac768a264a1d8052e8058
GeekyOg <oghomenag@gmail.com>
2026-02-21 03:24:31 +0100

    UI update

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import React from "react";
-import { deviceType, isiOS } from "../utils/checkDevice";
+// import { deviceType, isiOS } from "../utils/checkDevice";
 
 interface PrivateRouteProps {
   children: React.ReactNode;
@@ -11,7 +11,7 @@ interface PrivateRouteProps {
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const [user] = useState(Cookies.get("authToken"));
 
-  const device = deviceType();
+  // const device = deviceType();
   const navigate = useNavigate();
 
   useEffect(() => {
